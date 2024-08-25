f151f3a9cdf2fc72d824ff15a541344fc3c75804
Steve <stevedsimkins@gmail.com>
2024-08-25 16:24:04 -0400

    reorg

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/shirou/gopsutil/cpu"
@@ -69,7 +68,7 @@ func handleSSE(w http.ResponseWriter, r *http.Request) {
 	log.Println("SSE connection established")
 
 	for {
-		combinedStats, err := getIpfsStats()
+		combinedStats, err := getStats()
 		if err != nil {
 			log.Printf("Error getting IPFS Stats: %v", err)
 			time.Sleep(1 * time.Second)
@@ -94,7 +93,7 @@ func handleSSE(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getIpfsStats() (CombinedStats, error) {
+func getStats() (CombinedStats, error) {
 	repoStats, err := getIpfsRepoStat()
 	if err != nil {
 		return CombinedStats{}, err
@@ -198,40 +197,4 @@ func getSystemStats() (SystemStats, error) {
 		Uptime:      formatUptime(uptime),
 		CPUModel:    "BCM2835 (4) @ 1.800GHz",
 	}, nil
-}
-
-func formatBytes(bytes int64) string {
-	const unit = 1024
-	if bytes < unit {
-		return fmt.Sprintf("%d B", bytes)
-	}
-	div, exp := int64(unit), 0
-	for n := bytes / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-	return fmt.Sprintf("%.1f %ciB", float64(bytes)/float64(div), "KMGTPE"[exp])
-}
-
-func formatUptime(uptime time.Duration) string {
-	days := int(uptime.Hours() / 24)
-	hours := int(uptime.Hours()) % 24
-	minutes := int(uptime.Minutes()) % 60
-	seconds := int(uptime.Seconds()) % 60
-
-	var parts []string
-	if days > 0 {
-		parts = append(parts, fmt.Sprintf("%dd", days))
-	}
-	if hours > 0 {
-		parts = append(parts, fmt.Sprintf("%dh", hours))
-	}
-	if minutes > 0 {
-		parts = append(parts, fmt.Sprintf("%dm", minutes))
-	}
-	if seconds > 0 || len(parts) == 0 {
-		parts = append(parts, fmt.Sprintf("%ds", seconds))
-	}
-
-	return strings.Join(parts, " ")
 }
---
+++ utils.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func formatBytes(bytes int64) string {
+	const unit = 1024
+	if bytes < unit {
+		return fmt.Sprintf("%d B", bytes)
+	}
+	div, exp := int64(unit), 0
+	for n := bytes / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB", float64(bytes)/float64(div), "KMGTPE"[exp])
+}
+
+func formatUptime(uptime time.Duration) string {
+	days := int(uptime.Hours() / 24)
+	hours := int(uptime.Hours()) % 24
+	minutes := int(uptime.Minutes()) % 60
+	seconds := int(uptime.Seconds()) % 60
+
+	var parts []string
+	if days > 0 {
+		parts = append(parts, fmt.Sprintf("%dd", days))
+	}
+	if hours > 0 {
+		parts = append(parts, fmt.Sprintf("%dh", hours))
+	}
+	if minutes > 0 {
+		parts = append(parts, fmt.Sprintf("%dm", minutes))
+	}
+	if seconds > 0 || len(parts) == 0 {
+		parts = append(parts, fmt.Sprintf("%ds", seconds))
+	}
+
+	return strings.Join(parts, " ")
+}
